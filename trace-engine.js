cc5d999d544517ab18c44d280c71f490c723ab34
Hugo Castaneda <hugodecasta@orange.fr>
2018-12-27 22:06:36 +0100

    create a trace-engine, not tested yet

---
+++ trace-engine.js
@@ -0,0 +1,147 @@
+//-----------------------------------------------------------------------------
+// Copyright (C) 2019 Hugo Castaneda
+// Licensed under the MIT license.
+// See LICENSE.md file in the project root for full license information.
+//-----------------------------------------------------------------------------
+
+'use strict'
+
+// ----------------------------------------------------------------------------
+// ------------------------------------------------------------ IMPORTS -------
+
+const fs = require('fs-promise')
+
+// ----------------------------------------------------------------------------
+// ------------------------------------------------------------- DESIGN -------
+
+/*
+*
+* Trace representation:
+*   - name: string
+*   - description: string
+*   - lastUpdate: timestamp
+*
+*/
+
+// ----------------------------------------------------------------------------
+// ---------------------------------------------------------- INTERFACE -------
+
+// -----------------------------------------------------
+exports.traceExists = async function(traceName) {
+  return await traceExists(traceName)
+}
+
+exports.getTraceInfos = async function(traceName) {
+  if(! await traceExists(traceName))
+    return null
+  return await retrieveInfos(traceName)
+}
+
+exports.getTraceObsels = async function(traceName) {
+  if(! await traceExists(traceName))
+    return null
+  return await retrieveObsels(traceName)
+}
+
+// -----------------------------------------------------
+exports.createTrace = async function(traceName, description) {
+  if(await traceExists(traceName))
+    return false
+  await createTrace(traceName, description)
+  return true
+}
+
+exports.addObsel = async function(traceName, obsel) {
+  if(! await traceExists(traceName))
+    return false
+  await addObsel(traceName, obsel)
+  return true
+}
+
+// -----------------------------------------------------
+exports.deleteTrace = async function(traceName) {
+  if(! await traceExists(traceName))
+    return false
+  await deleteTrace(traceName)
+  return true
+}
+
+// ----------------------------------------------------------------------------
+// ----------------------------------------------------------- INTERNAL -------
+
+// --------------------------------
+async function traceExists(traceName) {
+  return await fs.exists(traceInfoFile(traceName))
+}
+
+// --------------------------------
+async function createTrace(traceName, description) {
+  let infos = {
+    name: traceName,
+    description: description,
+    lastUpdate: await getNowTime()
+  }
+  await saveInfos(traceName, infos)
+  await saveObsels(traceName, [])
+}
+
+async function addObsel(traceName, obsel) {
+  let obsels = await retrieveObsels(traceName)
+  obsels.push(obsel)
+  await saveObsels(traceName, obsels)
+  let infos = await retrieveInfos(traceName)
+  infos.lastUpdate = await getNowTime()
+  await saveInfos(traceName, infos)
+}
+
+// --------------------------------
+async function deleteTrace(traceName) {
+  let infoFile = await traceInfoFile(traceName)
+  let obselFile = await traceObselFile(traceName)
+  await fs.unlink(infoFile)
+  await fs.unlink(obselFile)
+}
+
+// ----------------------------------------------------- DIR
+
+// --------------------------------
+async function retrieveInfos(traceName) {
+  let infoFile = await traceInfoFile(traceName)
+  let infos = JSON.parse(await fs.readFile(infoFile))
+  return infos
+}
+
+async function saveInfos(traceName, infos) {
+  let infoFile = await traceInfoFile(traceName)
+  await fs.writeFile(infoFile, JSON.stringify(infos))
+}
+
+// --------------------------------
+async function retrieveObsels(traceName) {
+  let obselFile = await traceObselFile(traceName)
+  let obsels = JSON.parse(await fs.readFile(obselFile))
+  return obsels
+}
+
+async function saveObsels(traceName, obsels) {
+  let obselFile = await traceObselFile(traceName)
+  await fs.writeFile(obselFile, JSON.stringify(obselObject))
+}
+
+// --------------------------------
+async function traceInfoFile(traceName) {
+  return traceDirectory() + traceName + '.json'
+}
+
+async function traceObselFile(traceName) {
+  return traceDirectory() + traceName + '_obsels.json'
+}
+
+async function traceDirectory() {
+  return './traces/'
+}
+
+// ----------------------------------------------------- UTILS
+async function getNowTime() {
+  return Math.floor(Date.now() / 1000)
+}
